54c5ce9219144219ce37faef7e3428dd7e3eb955
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-01-10 13:59:25 -0500

    boton mute ok

--- assets/index.js
+++ assets/index.js
@@ -2,8 +2,17 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
-
-
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
-button.onclick = () => player.togglePlay();
+
+const button = document.querySelector("#playButton");
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector("#muteButton");
+muteButton.onclick = () => {
+    if(player.media.muted) {
+        player.unmute();
+    } else {
+        player.mute();
+    }
+}
+
