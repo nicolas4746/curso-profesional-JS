8d1281f84edf3269daa3f924cbee9f1b4b2cbf42
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-01-21 21:21:45 -0500

    se cambian todos los archivos a typescript

---
+++ ejercicios/typescrip/dist/index.js
@@ -0,0 +1,5 @@
+console.log('Hello, TypeScript');
+function add(a, b) {
+    return a + b;
+}
+var sum = add(2, 3);
--- ejercicios/typescrip/index.ts
+++ ejercicios/typescrip/index.ts
@@ -1,7 +1,9 @@
 console.log('Hello, TypeScript');
 
-function add(a: number, b: number) {
+function add(a: number, b: number):number {
   return a + b;
 }
 
-const sum = add(2, 3);
+const sumatoria = add(2,3);
+
+console.log(sumatoria);
